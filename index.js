f7b573211ba93ae60f569f1727cab07f768d59c4
Developer Hope <me@devhope.co>
2021-03-10 18:07:17 +0000

    Adding text inputs to a new div below

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ let entriesSection = document.getElementById("entries");
 let entryTextbox = document.getElementsByClassName("entry-textbox");
 
 function addEntryToDom(event) {
+ let entryDiv = document.createElement("div");
+ entryDiv.classList.add("single-entry");
+ entryDiv.innerText = entryTextbox[0].value;
+ entriesSection.appendChild(entryDiv);
+ console.log(entryDiv.innerText);
  event.preventDefault();
- console.log(entryTextbox[0].value);
+ entryTextbox[0].value = "";
 }
 
 entryForm.addEventListener("submit", addEntryToDom)
