76b4bb1856f70266a7df43e16c9edb7a18902c69
Developer Hope <me@devhope.co>
2021-03-15 17:31:50 +0000

    Improved JS file, cleaned console.log and organised actions

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ let entriesSection = document.getElementById("entries");
 let entryTextbox = document.getElementsByClassName("entry-textbox");
 
 function addEntryToDom(event) {
+ event.preventDefault();
  let entryDiv = document.createElement("div");
  entryDiv.classList.add("single-entry");
  entryDiv.innerText = entryTextbox[0].value;
  entriesSection.appendChild(entryDiv);
- console.log(entryDiv.innerText);
- event.preventDefault();
  entryTextbox[0].value = "";
 }
 
