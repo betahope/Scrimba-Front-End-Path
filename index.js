5aa11c07469952b2185ad13b2cda4fdd7be77d72
Developer Hope <me@devhope.co>
2021-03-18 18:12:19 +0000

    Adding an HTML button with every submission

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let entryForm = document.getElementById("entryForm");
-let entriesSection = document.getElementById("entries");
-let entryTextbox = document.getElementsByClassName("entry-textbox");
+let entriesSection = document.querySelector("#entries");
+let entryTextbox = document.querySelector(".entry-textbox");
+let entriesNav = document.querySelector(".entries-nav");
 
 function addEntryToDom(event) {
  event.preventDefault();
  let entryDiv = document.createElement("div");
  entryDiv.classList.add("single-entry");
- entryDiv.innerText = entryTextbox[0].value;
+ entryDiv.innerText = entryTextbox.value;
  entryDiv.style.display = "none";
  entriesSection.appendChild(entryDiv);
- entryTextbox[0].value = "";
+ entryTextbox.value = "";
+
+ let displayEntryButton = document.createElement("button");
+ displayEntryButton.className = "display-entry-button";
+ displayEntryButton.innerText = 1;
+ entriesNav.appendChild(displayEntryButton);
 }
 
 entryForm.addEventListener("submit", addEntryToDom)
